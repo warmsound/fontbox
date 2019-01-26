03a0f13d6665c532359090336e78dc89da4690f1
Vince Wong <mail.vince@gmail.com>
2019-01-26 18:12:59 +0000

    Use alpha channel data only: simplifies calc*() functions

--- demo/src/app.js
+++ demo/src/app.js
@@ -24,92 +24,95 @@ class Demo {
 			CANVAS_SIZE - MARGIN);
 		//ctx.fillRect(30, 30, 60, 60);
 
-		let glyphPixels = ctx.getImageData(0, 0, CANVAS_SIZE, CANVAS_SIZE);
-		let bgDistPixels = ctx.createImageData(CANVAS_SIZE, CANVAS_SIZE);
-		let localMaximaPixels = ctx.createImageData(CANVAS_SIZE, CANVAS_SIZE);
-		this.calcBgDist(glyphPixels, bgDistPixels);
-		this.drawBgDist(bgDistPixels);
-		this.calcLocalMaxima(bgDistPixels, localMaximaPixels);
-		this.drawLocalMaxima(localMaximaPixels);
+		let glyphData = ctx.getImageData(0, 0, CANVAS_SIZE, CANVAS_SIZE);
+		let width = glyphData.width;
+		let height = glyphData.height;
+
+		// Reduce glyphData to array of alpha channel values.
+		let glyphPixels = glyphData.data.filter((element, index, array) => !((index - 3) % 4));
+		let bgDistPixels = new Uint8ClampedArray(glyphPixels.length);		
+		
+		let maxBgDist = this.calcBgDist(glyphPixels, bgDistPixels, width, height);
+		this.drawBgDist(bgDistPixels, width, height, maxBgDist);
+
+		let localMaximaPixels = new Uint8ClampedArray(glyphPixels.length);
+		let maximaFreq = this.calcLocalMaxima(bgDistPixels, localMaximaPixels, width, height);
+		this.drawLocalMaxima(localMaximaPixels, width, height, maxBgDist);
 	}
 
-	calcBgDist(src, dst) {
-		let s = src.data;
-
-		const row = (src.width * 4);
+	calcBgDist(src, dst, w, h) {
 		const ROOT_2 = Math.pow(2, 0.5);
 
-		// x and y are pixel co-ords. Traverse each row before moving down to next.
-		for (let y = 0; y < src.height; ++y) {
-			for (let x = 0; x < src.width; ++x) {
+		let maxDist = 0;
 
-				// i is index into pixel data array for pixel at (x, y).
-				let i = ((y * src.width) + x) * 4;
+		let i; // Index for pixel at (x, y).
+		let j; // Index for search pixel.
+		let bgFound;
 
-				// j is index into pixel data array for search pixel.
-				let j;
-
-				let bgFound = false;
+		for (let y = 0; y < h; ++y) {
+			for (let x = 0; x < w; ++x) {
+				i = x + (y * w);
+				bgFound = false;
 
 				// If pixel is not fully transparent.
-				if (s[i + 3]) {
+				if (src[i]) {
 
 					// Determine minimum pixel distance from fully transparent background pixel.
 					let dist = CANVAS_SIZE;
 					for (let d = 1; d < dist; ++d) {
 
 						// Search north: (x, y - d).
-						j = (((y - d) * src.width) + x) * 4;
-						if ((y - d) < 0 || s[j] == 0) {
+						j = x + ((y - d) * w);
+						if ((y - d) < 0 || src[j] == 0) {
 							dist = Math.min(dist, d);
 							bgFound = true;
 						}
 
 						// Search north-east: (x + d, y - d).
-						j = (((y - d) * src.width) + x + d) * 4;
-						if ((x + d) >= src.width || (y - d) < 0 || s[j] == 0) {
+						j = (x + d) + ((y - d) * w);
+						if ((x + d) >= w || (y - d) < 0 || src[j] == 0) {
 							dist = Math.min(dist, d * ROOT_2);
 							bgFound = true;
 						}
 
 						// Search east: (x + d, y).
-						j = ((y * src.width) + x + d) * 4;
-						if ((x + d) >= src.width || s[j] == 0) {
+						j = (x + d) + (y * w);
+						if ((x + d) >= w || src[j] == 0) {
 							dist = Math.min(dist, d);
 							bgFound = true;
 						}
 
 						// Search south-east: (x + d, y + d).
-						j = (((y + d) * src.width) + x + d) * 4;
-						if ((x + d) >= src.width || (y + d) >= src.height || s[j] == 0) {
+						j = (x + d) + ((y + d) * w);
+						if ((x + d) >= w || (y + d) >= h || src[j] == 0) {
 							dist = Math.min(dist, d * ROOT_2);
 							bgFound = true;
 						}
 
 						// Search south: (x, y + d).
-						j = (((y + d) * src.width) + x) * 4;
-						if (((y + d) >= src.height) || s[j] == 0) {
+						j = x + ((y + d) * w);
+						if (((y + d) >= h) || src[j] == 0) {
 							dist = Math.min(dist, d);
 							bgFound = true;
 						}
 
 						// Search south-west: (x - d, y + d).
-						j = (((y + d) * src.width) + x - d) * 4;
-						if ((x - d) < 0 || (y + d) >= src.height || s[j] == 0) {
+						j = (x - d) + ((y + d) * w);
+						if ((x - d) < 0 || (y + d) >= h || src[j] == 0) {
 							dist = Math.min(dist, d * ROOT_2);
 							bgFound = true;
 						}
 
 						// Search west: (x - d, y).
-						j = ((y * src.width) + x - d) * 4;
-						if (((x - d) < 0) || s[j] == 0) {
+						j = (x - d) + (y * w);
+						if (((x - d) < 0) || src[j] == 0) {
 							dist = Math.min(dist, d);
 							bgFound = true;
 						}
 
 						// Search north-west: (x - d, y - d).
-						j = (((y - d) * src.width) + x - d) * 4;
-						if ((x - d) < 0 || (y - d) >= src.height || s[j] == 0) {
+						j = (x - d) + ((y - d) * w);
+						if ((x - d) < 0 || (y - d) >= h || src[j] == 0) {
 							dist = Math.min(dist, d * ROOT_2);
 							bgFound = true;
 						}
@@ -118,106 +121,110 @@ class Demo {
 					// Background or edge was found.
 					// Write fully-opaque greyscale value that represents distance of pixel from background.
 					if (bgFound) {
-						dst.data[i] = dst.data[i + 1] = dst.data[i + 2] = dist;
-						dst.data[i + 3] = 255;
+						dst[i] = dist;
+						maxDist = Math.max(dist, maxDist);
 					}
 				}
 			}
 		}
+
+		return maxDist;
 	}
 
-	drawBgDist(pixels) {
+	drawBgDist(pixels, w, h, max) {
 		let ctx = document.getElementById('bg-dist-canvas').getContext('2d');
-		ctx.putImageData(pixels, 0, 0);
+		let data = ctx.createImageData(w, h);
+		for (let i = 0; i < pixels.length; ++i) {
+			data.data[(4 * i) + 0] = data.data[(4 * i) + 1] = data.data[(4 * i) + 2] = (pixels[i] / max) * 255;
+			data.data[(4 * i) + 3] = 255;
+		}
+		ctx.putImageData(data, 0, 0);
 	}
 
-	calcLocalMaxima(src, dst) {
-		let s = src.data;
-
-		const row = (src.width * 4);
-		const ROOT_2 = Math.pow(2, 0.5);
-
+	calcLocalMaxima(src, dst, w, h) {
 		let localMaxima = [];
 
+		let i; // Index for pixel at (x, y).
+		let j; // Index for search pixel.
+
 		// x and y are pixel co-ords. Traverse each row before moving down to next.
-		for (let y = 0; y < src.height; ++y) {
-			for (let x = 0; x < src.width; ++x) {
-
-				// i is index into pixel data array for pixel at (x, y).
-				let i = ((y * src.width) + x) * 4;
-
-				// j is index into pixel data array for search pixel.
-				let j;
+		for (let y = 0; y < h; ++y) {
+			for (let x = 0; x < w; ++x) {
+				i = x + (y * w);
 
 				// If pixel is not fully transparent.
-				if (s[i + 3]) {
+				if (src[i]) {
 
 					// Search north: (x, y - 1).
-					j = (((y - 1) * src.width) + x) * 4;
-					if ((y - 1) >= 0 && s[j] > s[i]) {
+					j = x + ((y - 1) * w);
+					if ((y - 1) >= 0 && src[j] > src[i]) {
 						continue;
 					}
 
 					// Search north-east: (x + 1, y - 1).
-					j = (((y - 1) * src.width) + x + 1) * 4;
-					if ((x + 1) < src.width && (y - 1) >= 0 && s[j] > s[i]) {
+					j = (x + 1) + ((y - 1) * w);
+					if ((x + 1) < w && (y - 1) >= 0 && src[j] > src[i]) {
 						continue;
 					}
 
 					// Search east: (x + 1, y).
-					j = ((y * src.width) + x + 1) * 4;
-					if ((x + 1) < src.width && s[j] > s[i]) {
+					j = (x + 1) + (y * w);
+					if ((x + 1) < w && src[j] > src[i]) {
 						continue;
 					}
 
 					// Search south-east: (x + 1, y + 1).
-					j = (((y + 1) * src.width) + x + 1) * 4;
-					if ((x + 1) < src.width && (y + 1) < src.height && s[j] > s[i]) {
+					j = (x + 1) + ((y + 1) * w);
+					if ((x + 1) < w && (y + 1) < h && src[j] > src[i]) {
 						continue
 					}
 
 					// Search south: (x, y + 1).
-					j = (((y + 1) * src.width) + x) * 4;
-					if (((y + 1) < src.height) && s[j] > s[i]) {
+					j = x + ((y + 1) * w);
+					if (((y + 1) < h) && src[j] > src[i]) {
 						continue;
 					}
 
 					// Search south-west: (x - 1, y + 1).
-					j = (((y + 1) * src.width) + x - 1) * 4;
-					if ((x - 1) >= 0 && (y + 1) < src.height && s[j] > s[i]) {
+					j = (x - 1) + ((y + 1) * w);
+					if ((x - 1) >= 0 && (y + 1) < h && src[j] > src[i]) {
 						continue
 					}
 
 					// Search west: (x - 1, y).
-					j = ((y * src.width) + x - 1) * 4;
-					if (((x - 1) >= 0) && s[j] > s[i]) {
+					j = (x - 1) + (y * w);
+					if (((x - 1) >= 0) && src[j] > src[i]) {
 						continue;
 					}
 
 					// Search north-west: (x - 1, y - 1).
-					j = (((y - 1) * src.width) + x - 1) * 4;
-					if ((x - 1) >= 0 && (y - 1) < src.height && s[j] > s[i]) {
+					j = (x - 1) + ((y - 1) * w);
+					if ((x - 1) >= 0 && (y - 1) < h && src[j] > src[i]) {
 						continue;
 					}
 
-					dst.data[i] = dst.data[i + 1] = dst.data[i + 2] = s[j];
-					dst.data[i + 3] = 255;
-					localMaxima.push(s[j]);
+					dst[i] = src[i];
+					localMaxima.push(src[j]);
 				}
 			}
 		}
 
-		let maximaCounts = {};
+		let maximaFreq = {};
 		localMaxima.forEach(maxima => {
-			maximaCounts[maxima] = maximaCounts[maxima] ? maximaCounts[maxima] + 1 : 1;
+			maximaFreq[maxima] = maximaFreq[maxima] ? maximaFreq[maxima] + 1 : 1;
 		});
-		console.log(maximaCounts);
-		return maximaCounts;
+		console.log(maximaFreq);
+		return maximaFreq;
 	}
 
-	drawLocalMaxima(pixels) {
+	drawLocalMaxima(pixels, w, h, max) {
 		let ctx = document.getElementById('local-maxima-canvas').getContext('2d');
-		ctx.putImageData(pixels, 0, 0);
+		let data = ctx.createImageData(w, h);
+		for (let i = 0; i < pixels.length; ++i) {
+			data.data[(4 * i) + 0] = data.data[(4 * i) + 1] = data.data[(4 * i) + 2] = (pixels[i] / max) * 255;
+			data.data[(4 * i) + 3] = 255;
+		}
+		ctx.putImageData(data, 0, 0);
 	}
 }
 
